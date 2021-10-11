db59e36cdc6164cf3b8e2bbb09ef7fda0767dfa9
Tonia Hurst <51763259+toniahurst@users.noreply.github.com>
2021-10-11 13:50:13 -0700

    Fixed part of error. Chatbot working

--- Starter_Code_module_15/Lambda/lambda_function.py
+++ Starter_Code_module_15/Lambda/lambda_function.py
@@ -80,37 +80,6 @@ def close(session_attributes, fulfillment_state, message):
     return response
 
 
-"""
-Step 3: Enhance the Robo Advisor with an Amazon Lambda Function
-
-In this section, you will create an Amazon Lambda function that will validate the data provided by the user on the Robo Advisor.
-
-1. Start by creating a new Lambda function from scratch and name it `recommendPortfolio`. Select Python 3.7 as runtime.
-
-2. In the Lambda function code editor, continue by deleting the AWS generated default lines of code, then paste in the starter code provided in `lambda_function.py`.
-
-3. Complete the `recommend_portfolio()` function by adding these validation rules:
-
-    * The `age` should be greater than zero and less than 65.
-    * The `investment_amount` should be equal to or greater than 5000.
-
-4. Once the intent is fulfilled, the bot should respond with an investment recommendation based on the selected risk level as follows:
-
-    * **none:** "100% bonds (AGG), 0% equities (SPY)"
-    * **low:** "60% bonds (AGG), 40% equities (SPY)"
-    * **medium:** "40% bonds (AGG), 60% equities (SPY)"
-    * **high:** "20% bonds (AGG), 80% equities (SPY)"
-
-> **Hint:** Be creative while coding your solution, you can have all the code on the `recommend_portfolio()` function, or you can split the functionality across different functions, put your Python coding skills in action!
-
-5. Once you finish coding your Lambda function, test it using the sample test events provided for this Challenge.
-
-6. After successfully testing your code, open the Amazon Lex Console and navigate to the `recommendPortfolio` bot configuration, integrate your new Lambda function by selecting it in the “Lambda initialization and validation” and “Fulfillment” sections.
-
-7. Build your bot, and test it with valid and invalid data for the slots.
-
-"""
-
 
 ### Intents Handlers ###
 def recommend_portfolio(intent_request):
@@ -123,9 +92,44 @@ def recommend_portfolio(intent_request):
     investment_amount = get_slots(intent_request)["investmentAmount"]
     risk_level = get_slots(intent_request)["riskLevel"]
     source = intent_request["invocationSource"]
+    
+    if source == "DialogCodeHook":
+        slots = get_slots(intent_request)
+        # Fetch current session attributes
+        output_session_attributes = intent_request["sessionAttributes"]
 
-    # YOUR CODE GOES HERE!
+        # Once all slots are valid, a delegate dialog is returned to Lex to choose the next course of action.
+        return delegate(output_session_attributes, get_slots(intent_request))
+    
+    if parse_int(age) != None and parse_int(age) < 0 or parse_int(age) >= 65:
+        return build_validation_result(
+            False,
+            "age",
+            "Please restart, the age entered is invalid"
+            )
+            
+    elif parse_int(investment_amount) == None or parse_int(investment_amount) < 5000:
+        return build_validation_result(
+            False,
+            "investment_amount",
+            "The minimum investment threshold is $5000"
+            )
+    recommendation = ""        
+    if risk_level == "None":
+        recommendation = "100% bonds (AGG), 0% equities (SPY)"
+            
+    elif risk_level == "Low":
+        recommendation = "60% bonds (AGG), 40% equities (SPY)"
 
+    elif risk_level == "Medium":
+        recommendation = "40% bonds (AGG), 60% equities (SPY)"
+
+    elif risk_level == "High":
+        recommendation = "20% bonds (AGG), 80% equities (SPY)"
+            
+    return close(intent_request["sessionAttributes"], 
+        "Fulfilled",
+        message = recommendation)
 
 ### Intents Dispatcher ###
 def dispatch(intent_request):
